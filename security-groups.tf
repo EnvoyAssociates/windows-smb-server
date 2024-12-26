e1a0bcd238a998ebe6e7b5d5af03132344c2664a
gerrardrose <gerrard.rose@rebura.com>
2024-12-26 15:53:38 +0000

    add security groups

--- security-groups.tf
+++ security-groups.tf
@@ -1,4 +1,100 @@
 #########################
+# WireGuard VPN
+#########################
+
+module "wireguard_vpn_sg" {
+  source = "cloudposse/security-group/aws"
+  version = "2.2.0"
+  
+  # attributes = ["${var.account_type}-${var.env}", "wireguard-sg"]
+  security_group_description = "SMB File Server Security Group"
+  vpc_id  = module.vpc.vpc_id
+  allow_all_egress = true
+
+  rules = [
+    {
+      key         = "allow_all"
+      type        = "ingress"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = []
+      self        = true
+      description = "Inbound from all resources in this SG"
+    },
+    {
+      key         = "wireguard_envoy"
+      type        = "ingress"
+      from_port   = 51820
+      to_port     = 51820
+      protocol    = "udp"
+      cidr_blocks = ["157.231.92.5/32"]
+      self        = null
+      description = "Wireguard from Envoy Office"
+    },
+    {
+      key         = "smb_envoy"
+      type        = "ingress"
+      from_port   = 445
+      to_port     = 445
+      protocol    = "tcp"
+      cidr_blocks = ["192.168.1.0/24"]
+      self        = null
+      description = "SMB from Envoy Office"
+    },
+    {
+      key         = "ssh_gr_home"
+      type        = "ingress"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["154.59.148.74/32"]
+      self        = null
+      description = "SSH from GR home"
+    }
+  ]
+
+  # rules_map = {
+  #   ingress = [
+  #   {
+  #     key         = "sftp_mule_server"
+  #     type        = "ingress"
+  #     from_port   = 22
+  #     to_port     = 22
+  #     protocol    = "tcp"
+  #     source_security_group_id = module.mule_sg.id
+  #     self        = null
+  #     description = "Mule Server Security Group"
+  #   },
+  #   {
+  #     key         = "sftp_websphere_server"
+  #     type        = "ingress"
+  #     from_port   = 22
+  #     to_port     = 22
+  #     protocol    = "tcp"
+  #     source_security_group_id = module.websphere_sg.id
+  #     self        = null
+  #     description = "Websphere Server Security Group"
+  #   },
+  #   {
+  #     key         = "sftp_db_server"
+  #     type        = "ingress"
+  #     from_port   = 22
+  #     to_port     = 22
+  #     protocol    = "tcp"
+  #     source_security_group_id = module.db_sg.id
+  #     self        = null
+  #     description = "Database Server Security Group"
+  #   }
+  #   ]
+  # }
+
+  create_before_destroy = true
+  preserve_security_group_id = true
+}
+
+
+#########################
 # SMB File Server
 #########################
 
@@ -6,7 +102,7 @@ module "smb_fileserver_sg" {
   source = "cloudposse/security-group/aws"
   version = "2.2.0"
   
-  attributes = ["${var.account_type}-${var.env}", "sftp-sg"]
+  # attributes = ["${var.account_type}-${var.env}", "smb-sg"]
   security_group_description = "SMB File Server Security Group"
   vpc_id  = module.vpc.vpc_id
   allow_all_egress = true
---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "account_type" {
+  type        = string
+  description = "account_type"
+  default = "envoy"
+}
+
+variable "env" {
+  type        = string
+  description = "Environment name"
+  default = "prod"
+}
