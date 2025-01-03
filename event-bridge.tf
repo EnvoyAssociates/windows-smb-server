1739b74c3d7aca909e57c1ba697db53ef8b11390
gerrardrose <gerrard.rose@rebura.com>
2025-01-03 09:24:01 +0000

    update daily shutdown

--- event-bridge.tf
+++ event-bridge.tf
@@ -1,11 +1,9 @@
 
 locals {
   weekday_start = "cron(0 7 ? * MON-FRI *)"
-  weekday_stop  = "cron(0 19 ? * MON-FRI *)"
+  weekday_stop  = "cron(0 19 ? * MON-SUN *)"
   weekend_start = "cron(0 9 ? * SAT *)"
   weekend_stop  = "cron(0 13 ? * SAT *)"
-  start_now     = "cron(30 11 31 12 ? 2024)"  # Fields: minute, hour, day of month, month, ?, year
-  stop_now      = "cron(40 11 31 12 ? 2024)"
 }
 
 #########################
@@ -36,18 +34,6 @@ resource "aws_cloudwatch_event_target" "ec2_weekend_start" {
   arn       = module.lambda_ec2_start.arn
 }
 
-// EC2 start NOW!
-resource "aws_cloudwatch_event_rule" "ec2_start_now" {
-  name                = "schedule-${module.lambda_ec2_start.function_name}-now"
-  schedule_expression = local.start_now
-}
-
-resource "aws_cloudwatch_event_target" "ec2_start_now" {
-  target_id = "event-target-${module.lambda_ec2_start.function_name}"
-  rule      = aws_cloudwatch_event_rule.ec2_start_now.name
-  arn       = module.lambda_ec2_start.arn
-}
-
 #########################
 # EC2 Scheduled Stop
 #########################
@@ -73,17 +59,5 @@ resource "aws_cloudwatch_event_rule" "ec2_weekend_stop" {
 resource "aws_cloudwatch_event_target" "ec2_weekend_stop" {
   target_id = "event-target-${module.lambda_ec2_stop.function_name}"
   rule      = aws_cloudwatch_event_rule.ec2_weekend_stop.name
-  arn       = module.lambda_ec2_stop.arn
-}
-
-// EC2 start NOW!
-resource "aws_cloudwatch_event_rule" "ec2_stop_now" {
-  name                = "schedule-${module.lambda_ec2_stop.function_name}-now"
-  schedule_expression = local.stop_now
-}
-
-resource "aws_cloudwatch_event_target" "ec2_stop_now" {
-  target_id = "event-target-${module.lambda_ec2_stop.function_name}"
-  rule      = aws_cloudwatch_event_rule.ec2_stop_now.name
   arn       = module.lambda_ec2_stop.arn
 }
